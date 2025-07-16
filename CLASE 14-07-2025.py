b6be37ef7adab330bc70b9c56a6895499f1bb29d
Brand <vasbran111@gmail.com>
2025-07-16 00:11:45 -0500

    .

--- CLASE 14-07-2025.py
+++ CLASE 14-07-2025.py
@@ -1,25 +1,34 @@
-estudiantes ={}
-cantidad = int(input(f"\nIngrese cantidad de estudiantes"))
+productos ={}
+print("BIENVENIDO AL INVENTARIO DE ROPA")
+cantidad = int(input(f"\nIndique la cantidad de productos que desea agregar: "))
 for i in range(cantidad):
-    print(f"\n estudiante #{i+1}")
-    carne = input("Ingrese carnet: ")
-    nombre= input("Ingrese: ")
-    edad = int(input("ingrese edad: "))
-    carrera = input("Ingrese carrera: ")
-    estudiantes[carne]={
-        "nombre" : nombre,
-        "edad" : edad,
-        "carrera" : carrera
+    print(f"\n Producto #{i+1}")
+    codigo = int(input("Ingrese el codigo del producto: "))
+    nombre= input("Ingrese el nombre del producto: ")
+    categoria = input("ingrese la categoria del producto: ")
+    talla = input("Ingrese la talla: ")
+    precio = float(input("Ingrese el precio del producto: "))
+    stock = int(input("Ingrese stock: "))
+    productos[codigo]={
+        'nombre' : nombre,
+        'categoria' : categoria,
+        'talla' : talla,
+        'precio' : precio,
+        'stock' : stock
     }
-print("\nLista de estudiantes")
-for carne, datos in estudiantes.items():
-       print(f"Carne : {carne}")
-       print(f"Nombre: {datos[nombre]}")
-       print(f"Edad: {datos[edad]}")
-       print(f"Carrera: {datos[carrera]}")
-print("Buscar estudiante")
-buscado = input("Ingrese el numero de carne: ")
-if buscado in estudiantes:
-    print(f"Nombre: {estudiantes[buscado]["nombre"]}")
-    print(f"Edad: {estudiantes[buscado]["edad"]}")
-    print(f"Carrera: {estudiantes[buscado]["carrera"]}")
+print("\nLista de productos")
+for codigo, datos in productos.items():
+    print(f"Codigo: {codigo}")
+    print(f"Nombre: {datos['nombre']}")
+    print(f"Categoria: {datos['categoria']}")
+    print(f"Talla: {datos['talla']}")
+    print(f"Precio: {datos['precio']}")
+    print(f"Stock: {datos['stock']}")
+print("\nBuscar producto por codigo")
+buscado = int(input("Ingrese el codigo: "))
+if buscado in productos:
+    print(f"Nombre: {productos[buscado]['nombre']}")
+    print(f"Categoria: {productos[buscado]['categoria']}")
+    print(f"Talla: {productos[buscado]['talla']}")
+    print(f"Precio: {productos[buscado]['precio']}")
+    print(f"Stock: {productos[buscado]['stock']}")
